01dd930f254fccebed4672abdd0713fe70386441
v.khatchatrian <v.khatchatrian@groupeonepoint.com>
2025-05-09 16:20:13 +0200

    fix: update window title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Onestory",
+  description: "Log in with your corporate email to review user stories",
 };
 
 export default function RootLayout({
